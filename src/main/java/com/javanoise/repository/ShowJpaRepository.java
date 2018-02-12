300a4878cbfceaffd1da00136d493560a479e25c
daiot <stefanescu.dragos@gmail.com>
2018-02-12 21:55:41 +0200

    aded vue  but not fully working

--- src/main/java/com/javanoise/repository/ShowJpaRepository.java
+++ src/main/java/com/javanoise/repository/ShowJpaRepository.java
@@ -3,9 +3,11 @@ package com.javanoise.repository;
 import com.javanoise.model.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-//@RepositoryRestResource(collectionResourceRel = "shows", path = "shows")
+@CrossOrigin(origins = "http://localhost:8081")
+@RepositoryRestResource(collectionResourceRel = "shows", path = "shows")
 public interface ShowJpaRepository extends JpaRepository<Show,Long> {
     Show findByLink(String link);
 }
