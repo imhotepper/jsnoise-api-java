da04e37c5c3b5b6b8e77e62644deeed8af5e9753
daiot <stefanescu.dragos@gmail.com>
2018-11-20 22:48:06 +0200

    added minimal logging to the job

--- src/main/java/com/jsnoise/job/FeedUpdater.java
+++ src/main/java/com/jsnoise/job/FeedUpdater.java
@@ -17,12 +17,14 @@ import java.util.logging.Logger;
 @Component
 public class FeedUpdater {
 
+    private Logger _logger =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     @Autowired
     private JsNoiseService _service;
 
     @Scheduled(initialDelay = 1000, fixedRate = 2_160_000)
     public void update() {
-         _service.updateProducerShows();
+         long updatedPodcasts =_service.updateProducerShows();
+         _logger.info("Updated podcasts: " +  updatedPodcasts);
     }
 }
 
