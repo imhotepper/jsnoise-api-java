f90b5ff9fdc6a1d65ad0ad6ea251a7fed9e3d389
daiot <stefanescu.dragos@gmail.com>
2018-11-20 22:42:05 +0200

    swagger config added

--- src/main/java/com/jsnoise/config/SwaggerConfig.java
+++ src/main/java/com/jsnoise/config/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()             
           .apis(RequestHandlerSelectors.basePackage("com.jsnoise.controller"))                     
-          //.apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
+          .paths(PathSelectors.any())
           .build();                                           
     }
 }
+
